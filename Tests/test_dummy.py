fe02f45b255d2b37e79fe0064b06a49ca52c3f90
steffi <s.kieninger@fu-berlin.de>
2021-01-28 17:17:01 +0100

    Initial commit

---
+++ Tests/test_dummy.py
@@ -0,0 +1,7 @@
+from Code import dummy
+
+def test_dummy_function():
+    assert dummy.dummy_function()==1
+
+def test_sanity():
+    assert 1==1
---
+++ Tests/test_mama.py
@@ -0,0 +1,3 @@
+def test_mama():
+    assert 2==2
+
